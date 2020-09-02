2814437b31fbd67674f229eacb3369a2881064fc
Lucius (Xiao) Liu <51760338+lucius-xiao-liu@users.noreply.github.com>
2020-09-02 13:57:15 -0700

    Update 1484. Group Sold Products By The Date.sql

--- 1484. Group Sold Products By The Date.sql
+++ 1484. Group Sold Products By The Date.sql
@@ -6,3 +6,16 @@
 select sell_date, count(distinct product) as num_sold, group_concat(distinct product) as products
 from activities
 group by sell_date
+
+
+/* String_AGG-> SQL Server
+STRING_AGG ( expression, separator ) [ <order_clause> ]
+
+<order_clause> ::=   
+    WITHIN GROUP ( ORDER BY <order_by_expression_list> [ ASC | DESC ] ) 
+*/
+
+select t.sell_date, count(*) as num_sold, string_agg(product,',') as products
+from (select distinct * from activities) as t
+group by t.sell_date
+order by t.sell_date
