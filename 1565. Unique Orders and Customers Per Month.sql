e11a6ae510bb9a391fe1eee533d1d7971a8df17b
Lucius (Xiao) Liu <51760338+lucius-xiao-liu@users.noreply.github.com>
2020-09-02 14:06:03 -0700

    Update and rename 1565. Unique Orders and Customers Per Month to 1565. Unique Orders and Customers Per Month.sql

--- 1565. Unique Orders and Customers Per Month.sql
+++ 1565. Unique Orders and Customers Per Month.sql
@@ -3,3 +3,7 @@ select DATE_FORMAT(order_date, '%Y-%m') AS month, count(order_id) as order_count
 from orders
 where invoice>20
 group by month
+
+
+
+Using concat(year(order_date), '-',mont(order_date)) as month but formatting is different than the answer. Still a good solution
