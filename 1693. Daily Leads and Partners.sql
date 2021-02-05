198749aaf6b7eb0c1016871bc436c4a475e34153
Lucius (Xiao) Liu <51760338+lucius-xiao-liu@users.noreply.github.com>
2021-02-04 16:02:09 -0800

    Create 1693. Daily Leads and Partners.sql

---
+++ 1693. Daily Leads and Partners.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select date_id, make_name, count(distinct lead_id) as unique_leads, count(distinct partner_id) as unique_partners
+from dailysales
+group by date_id,make_name
