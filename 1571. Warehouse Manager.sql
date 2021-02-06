c22403d6002e207bcfaa5314d45dd81231b54032
Lucius (Xiao) Liu <51760338+lucius-xiao-liu@users.noreply.github.com>
2021-02-05 16:07:17 -0800

    Create 1571. Warehouse Manager.sql

--- 1571. Warehouse Manager.sql
+++ 1571. Warehouse Manager.sql
@@ -5,3 +5,17 @@ join (select p.product_id as product_id, p.product_name as product_name, (p.widt
      from products p
      group by product_id, product_name) p2 on w.product_id = p2.product_id
 group by warehouse_name
+
+
+
+# Write your MySQL query statement below
+with ware_prod as
+(select w.name as name, p.product_id, w.units as units, width*length*height as pro_volume
+from products p
+join
+warehouse w on p.product_id=w.product_id)
+ select name as warehouse_name,sum(pro_volume*units) as volume
+ from ware_prod
+ group by name
+ 
+ 
