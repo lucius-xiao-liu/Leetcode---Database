c540fd0d308def643a1eec1d731c0903ec3f1810
Lucius (Xiao) Liu <51760338+lucius-xiao-liu@users.noreply.github.com>
2021-02-08 15:27:08 -0800

    Update 1303. Find the Team Size.sql

--- 1303. Find the Team Size.sql
+++ 1303. Find the Team Size.sql
@@ -3,3 +3,8 @@ select employee_id, team_size
 from employee
 join (select team_id, count(*) as team_size from employee group by team_id) team
 on employee.team_id=team.team_id
+
+
+# Window Functions
+select employee_id, count(employee_id) over(partition by team_id) as team_size
+from Employee
