0f26418f90e9e1d69f3703ec3f26cec0a1739cb8
Lucius (Xiao) Liu <51760338+lucius-xiao-liu@users.noreply.github.com>
2021-02-08 15:22:34 -0800

    Update 1378. Replace Employee ID With The Unique Identifier.sql

--- 1378. Replace Employee ID With The Unique Identifier.sql
+++ 1378. Replace Employee ID With The Unique Identifier.sql
@@ -2,3 +2,10 @@
 select unique_id, name
 from employeeUNI
 right join Employees on EmployeeUNI.id=employees.id
+
+# Write your MySQL query statement below
+select unique_id,name
+from employeeuni
+right join employees
+using (id)
+
