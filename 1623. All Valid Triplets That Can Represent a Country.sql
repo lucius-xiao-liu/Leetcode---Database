78473b1bb8d1f348427c0296400e183b0f11ca56
Lucius (Xiao) Liu <51760338+lucius-xiao-liu@users.noreply.github.com>
2021-02-08 15:36:37 -0800

    Create 1623. All Valid Triplets That Can Represent a Country.sql

---
+++ 1623. All Valid Triplets That Can Represent a Country.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select a.student_name as member_A,b.student_name as member_B,c.student_name as member_C
+from schoola a
+cross join schoolb b
+cross join schoolc c
+where a.student_name <> b.student_name and b.student_name <> c.student_name and a.student_name <> c.student_name 
+and a.student_id <> b.student_id and b.student_id <> c.student_id and a.student_id <> c.student_id 
+
