889b5d57d5b8b2cd1cefafa268042d42d06cf0d4
Lucius (Xiao) Liu <51760338+lucius-xiao-liu@users.noreply.github.com>
2021-02-15 15:03:40 -0800

    Update 1327. List the Products Ordered in a Period.sql

--- 1327. List the Products Ordered in a Period.sql
+++ 1327. List the Products Ordered in a Period.sql
@@ -5,3 +5,14 @@ join orders on products.product_id=orders.product_id
 where order_date > '2020-01-31' and order_date<'2020-03-01'
 group by product_name
 having sum(unit) >99
+
+
+
+# Write your MySQL query statement below
+select product_name, sum(unit) as unit
+from products
+join orders
+using (product_id)
+where month(order_date)=2
+group by product_id
+having sum(unit)>=100
