d7b1e0c37687020de03c23fa5c4f8c376bc29f9f
Lucius (Xiao) Liu <51760338+lucius-xiao-liu@users.noreply.github.com>
2021-06-11 16:42:26 -0700

    Update 1142. User Activity for the Past 30 Days II.sql

--- 1142. User Activity for the Past 30 Days II.sql
+++ 1142. User Activity for the Past 30 Days II.sql
@@ -2,3 +2,15 @@
 select round(ifnull(count(distinct session_id)/count(distinct user_id),0),2) as average_sessions_per_user
 from activity
 where activity_date between '2019-06-28' and '2019-07-27'
+
+
+#date_sub
+select ifnull(round(sum(x)/count(user_id),2),0.00) as average_sessions_per_user 
+from 
+(
+select user_id, count(distinct session_id) as x
+from activity
+where 
+activity_date between date_sub('2019-07-27',interval 29 day) and '2019-07-27'
+group by user_id
+) as t 
