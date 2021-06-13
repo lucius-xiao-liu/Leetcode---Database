0599207077d00bc158f5d1a77bc47d23deceb4bd
Lucius (Xiao) Liu <51760338+lucius-xiao-liu@users.noreply.github.com>
2021-06-13 15:36:38 -0700

    Create 1398. Customers Who Bought Products A and B but Not C.sql

---
+++ 1398. Customers Who Bought Products A and B but Not C.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+select customer_id,customer_name
+from customers
+where customer_id in (select customer_id from orders where product_name = 'A')
+and customer_id in (select customer_id from orders where product_name = 'B')
+and customer_id not in (select customer_id from orders where product_name = 'C')
+group by 1,2
+
+
